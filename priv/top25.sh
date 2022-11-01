ac092040486ab3b7934bfc8f6ce4a309215b545c
Peter Hastie <pete@sillypog.com>
2022-11-01 11:51:42 -0700

    Additional montage options.

---
+++ priv/top25.sh
@@ -0,0 +1,2 @@
+montage 1.jpg 2.jpg 3.jpg 4.jpg 5.jpg 6.jpg 7.jpg 8.jpg 9.jpg 10.jpg 11.jpg 12.jpg 13.jpg 14.jpg 15.jpg 16.jpg 17.jpg 18.jpg 19.jpg 20.jpg 21.jpg 22.jpg 23.jpg 24.jpg 25.jpg -tile 5x5 -geometry 300x300+5+5 -background black top_25.png
+montage top_25.png -tile 1x1 -geometry +10+10 -background black top25_border.png
